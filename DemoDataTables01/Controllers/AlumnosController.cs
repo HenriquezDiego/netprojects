d7aa38773b423248001b433ce0a28d91608145bf
Diego Henriquez <df.henriquez@hotmail.com>
2018-09-14 17:34:33 -0500

    Manipulacion de DataTable y Scaffolding MVC5

---
+++ DemoDataTables01/Controllers/AlumnosController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoDataTables01.Models;
+using Microsoft.Ajax.Utilities;
+using System.Linq.Dynamic;
+
+namespace DemoDataTables01.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        // ------------------ Client Side ---------------------
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        public ActionResult GetList()
+        {
+            using (BDModels bd = new BDModels())
+            {
+                var aluList = bd.alu.ToList<alu>();
+                aluList[0].fecha_nac.ToString();
+                var jsonResult = Json(new { data = aluList }, JsonRequestBehavior.AllowGet);
+                jsonResult.MaxJsonLength = int.MaxValue;
+     
+                return jsonResult;
+            }
+        }
+        
+        // ------------------- Normal Way -----------------------
+        public ActionResult ServerList()
+        {
+            using (BDModels bd = new BDModels())
+            {
+                return View(bd.alu.ToList<alu>());
+            }
+        }
+
+        // --------------- Server Side Processing ---------------
+        public ActionResult ServerListAjax()
+        {
+            return View();
+        }
+                
+        [HttpPost]
+        public ActionResult GetListServer()
+        {
+            // Server Side Parameters
+            int start = Convert.ToInt32(Request["start"]);
+            int length = Convert.ToInt32(Request["length"]);
+            string searchValue = Request["search[value]"];
+            string sortColumnName = Request["columns[" + Request["order[0][column]"] + "][name]"];
+            string sortDirecction = Request["order[0][dir]"];
+                       
+            List<alu> aluListServer = new List<alu>();
+            using (BDModels bd = new BDModels())
+            {
+                aluListServer = bd.alu.ToList<alu>();
+                int totalRows = aluListServer.Count;
+                if (!string.IsNullOrEmpty(searchValue))//Filter
+                {
+                    
+                    aluListServer = aluListServer.
+                        Where(x => x.carnet.ToLower().Contains(searchValue.ToLower()) || x.apellido1.ToLower().Contains(searchValue.ToLower()) ||
+                        x.apellido2.ToLower().Contains(searchValue.ToLower()) || x.apellido3.ToLower().Contains(searchValue.ToLower()) || 
+                        x.nombre1.ToLower().Contains(searchValue.ToLower()) || x.nombre2.ToLower().Contains(searchValue.ToLower()) || 
+                        x.cod_carr.ToString().ToLower().Contains(searchValue.ToLower()) || x.activo.ToString().ToLower().Contains(searchValue.ToLower()) ||
+                        x.curso_asp.ToString().ToLower().Contains(searchValue.ToLower()) || x.proceso.ToString().ToLower().Contains(searchValue.ToLower()) ||
+                        x.fecha_pro.ToString().ToLower().Contains(searchValue.ToLower()) || x.fecha_nac.ToString().ToLower().Contains(searchValue.ToLower()) ||
+                        x.dui.ToLower().Contains(searchValue.ToLower()) || x.nit.ToLower().Contains(searchValue.ToLower()) ||
+                        x.recibo_ins.ToString().ToLower().Contains(searchValue.ToLower()) || x.ciclo_in.ToString().ToLower().Contains(searchValue.ToLower()) ||
+                        x.año_in.ToString().ToLower().Contains(searchValue.ToLower()) || x.cod_carr_old.ToString().ToLower().Contains(searchValue.ToLower()) ||
+                        x.carnet_old.ToLower().Contains(searchValue.ToLower()) || x.equi.ToString().ToLower().Contains(searchValue.ToLower()) ||
+                        x.sexo.ToString().ToLower().Contains(searchValue.ToLower())).ToList<alu>();
+
+                }
+                int totalRowsAfterFiltering = aluListServer.Count;
+                //Sorting
+                aluListServer = aluListServer.OrderBy(sortColumnName + " " + sortDirecction).ToList<alu>();
+
+                //Paging
+                aluListServer = aluListServer.Skip(start).Take(length).ToList<alu>();
+                
+                var jsonResult = Json(new { data = aluListServer, draw = Request["draw"], recordsTotal = totalRows, recordsFiltered = totalRowsAfterFiltering }, JsonRequestBehavior.AllowGet);
+                jsonResult.MaxJsonLength = int.MaxValue;
+
+                return jsonResult;            
+            }
+        }
+    }
+}
---
+++ DemoDataTables01/Controllers/alusController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DemoDataTables01.Models;
+
+namespace DemoDataTables01.Controllers
+{
+    public class alusController : Controller
+    {
+        private BDModels db = new BDModels();
+
+        // GET: alus
+        public ActionResult Index()
+        {
+            return View(db.alu.ToList());
+        }
+
+        // GET: alus/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            alu alu = db.alu.Find(id);
+            if (alu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alu);
+        }
+
+
+        // GET: alus/Detalle/5
+        public ActionResult Detalle(string id)
+        {
+            if (id == null)
+            {
+                //return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return RedirectToAction("Index", "alus");
+            }
+            alu alu = db.alu.Find(id);
+            if (alu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alu);
+        }
+
+
+
+
+
+        // GET: alus/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: alus/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "carnet,apellido1,apellido2,apellido3,nombre1,nombre2,cod_carr,activo,curso_asp,proceso,fecha_pro,fecha_nac,dui,nit,recibo_ins,ciclo_in,año_in,cod_carr_old,carnet_old,equi,sexo")] alu alu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.alu.Add(alu);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(alu);
+        }
+
+        // GET: alus/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            alu alu = db.alu.Find(id);
+            if (alu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alu);
+        }
+
+        // POST: alus/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "carnet,apellido1,apellido2,apellido3,nombre1,nombre2,cod_carr,activo,curso_asp,proceso,fecha_pro,fecha_nac,dui,nit,recibo_ins,ciclo_in,año_in,cod_carr_old,carnet_old,equi,sexo")] alu alu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(alu).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(alu);
+        }
+
+        // GET: alus/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            alu alu = db.alu.Find(id);
+            if (alu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alu);
+        }
+
+        // POST: alus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            alu alu = db.alu.Find(id);
+            db.alu.Remove(alu);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
